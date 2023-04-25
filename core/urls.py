a05a4fb9a43f7ee9aee4598bc868f35432f0ef8c
medeiroslucass <lcsmedeiroscosta@gmail.com>
2023-04-24 22:34:44 -0300

    checkpoint do projeto 24/04
    Signed-off-by: medeiroslucass <lcsmedeiroscosta@gmail.com>

--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.evaluation_view, name='experimento1'),
+    path('aaa/', views.my_view, name='my-view')
 ]
--- core/views.py
+++ core/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import render
-from .models import Audio
+from .models import Audio, Experimento
 
 def evaluation_view(request):
-    audios = Audio.objects.all()
+    experimento = Experimento.objects.get(id=1)
+    audios = Audio.objects.filter(category=experimento).order_by('id')
 
     if request.method == 'POST':
         print(request.POST)
     context = {
         'audios': audios,
+        'experimento': experimento,
     }
-    return render(request, 'experimento1.html', context)
+    return render(request, 'partials/base_form.html', context)
+
+
+def experimento2(request):
+    return render(request)
+def my_view(request):
+    return render(request, 'teste.html')
