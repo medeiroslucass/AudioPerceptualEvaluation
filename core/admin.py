b48ca2adf0ee5dc25013e5bad89d9c31dc2dec49
medeiroslucass <lcsmedeiroscosta@gmail.com>
2023-05-02 12:21:21 -0300

    Making Inline in Experimento model, now user can create an Object Experimento and at the same time create Audios and Rows.
    Signed-off-by: medeiroslucass <lcsmedeiroscosta@gmail.com>

--- core/admin.py
+++ core/admin.py
@@ -10,32 +10,39 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ('name_category', )
 
 admin.site.register(Category, CategoryAdmin)
+
+
 class EvaluationAdmin(admin.ModelAdmin):
     fields = ('audio', 'usuario', 'score', )
 
 admin.site.register(Evaluation, EvaluationAdmin)
 
-class ExperimentoAdmin(admin.ModelAdmin):
-    fields = ('name', 'description', )
-
-admin.site.register(Experimento, ExperimentoAdmin)
-
-
 
 class AudioInline(admin.TabularInline):
     model = Audio
-    extra = 0
+    extra = 1
+
+
+class ExperimentoAdmin(admin.ModelAdmin):
+    fields = ('name', 'description', )
+    inlines = [AudioInline]
+
+
+admin.site.register(Experimento, ExperimentoAdmin)
+
 
 class RowAdmin(admin.ModelAdmin):
     list_display = ('id', 'row')
     fields = ('row',)
     inlines = [AudioInline]
 
+
 admin.site.register(Row, RowAdmin)
 
 
-class AudioAdmin(admin.ModelAdmin):
-    list_display = ('id', 'archive', 'form', 'row')
-    fields = ('archive', 'form', 'row')
-
-admin.site.register(Audio, AudioAdmin)
+# class AudioAdmin(admin.ModelAdmin):
+#     list_display = ('id', 'archive', 'form', 'row')
+#     fields = ('archive', 'form', 'row')
+#
+#
+# admin.site.register(Audio, AudioAdmin)
